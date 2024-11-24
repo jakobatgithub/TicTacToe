2dc1f99a402ede203485fb625a619a05c6b5ed9a
Jakob <jakob@physik.tu-berlin.de>
2024-11-24 17:06:38 +0100

    Added type annotations.

--- DeepQAgent.py
+++ DeepQAgent.py
@@ -143,6 +143,7 @@ class DeepQLearningAgent(Agent):
 
             self.replay_buffer.add(state, action, reward, next_state, done)
             if len(self.replay_buffer) >= self.batch_size:
+                print("Training step")
                 states, actions, rewards, next_states, dones = self.replay_buffer.sample(self.batch_size)
 
                 q_values = self.q_network(states).gather(1, actions.unsqueeze(1)).squeeze(1)

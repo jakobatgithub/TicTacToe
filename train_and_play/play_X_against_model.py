ec212cc45330985a0c0b890da11c239c540fb846
Jakob <jakob@physik.tu-berlin.de>
2025-04-11 08:05:19 +0200

    Changed filenames. Added files for playing against trained model.

---
+++ train_and_play/play_X_against_model.py
@@ -0,0 +1,20 @@
+# import torch
+from TicTacToe.TicTacToe import TicTacToe
+from TicTacToe.DeepQAgent import DeepQPlayingAgent
+from TicTacToe.Agent import HumanAgent
+from TicTacToe.Display import ScreenDisplay
+
+# Load the model
+model_path = "./models/q_network_3x3x3_O.pth"  # Change this path to the desired model
+
+# Set up the game
+rows = 3
+win_length = 3
+agent1 = DeepQPlayingAgent(q_network=model_path, player="O")
+agent2 = HumanAgent(player="X")
+display = ScreenDisplay(rows=rows, cols=rows, waiting_time=0.5)
+
+game = TicTacToe(agent1, agent2, display=display, rows=rows, cols=rows, win_length=win_length)
+
+# Play the game
+game.play()

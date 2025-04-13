3179a2cd0e00b02cbe95d99751f01f9dbcbb27f8
Jakob <jakob@physik.tu-berlin.de>
2025-04-13 16:35:29 +0200

    Cosmetic changes.

--- train_and_play/train_model.py
+++ train_and_play/train_model.py
@@ -34,7 +34,7 @@ from TicTacToe.Evaluation import evaluate_performance
 from TicTacToe.TicTacToe import TicTacToe
 
 params: dict[str, Any] = {
-    "nr_of_episodes": 1000,  # number of episodes for training
+    "nr_of_episodes": 10000,  # number of episodes for training
     "rows": 3,  # rows of the board, rows = cols
     "epsilon_start": 0.75,  # initial exploration rate
     # "epsilon_min": 0.05,  # minimum exploration rate
@@ -52,7 +52,7 @@ params: dict[str, Any] = {
     "replay_buffer_length": 10000,  # replay buffer length
     "wandb": False,  # switch for logging with wandb.ai
     "wandb_logging_frequency": 25,  # wandb logging frequency
-    "load_network": False,  # file name for loading a PyTorch network
+    "load_network": True,  # file name for loading a PyTorch network
     "shared_replay_buffer": False,  # shared replay buffer
     "network_type": "FullyCNN",  # flag for network type, 'FCN' or 'CNN' or 'Equivariant' or 'FullyCNN'
     "periodic": True,  # periodic boundary conditions
@@ -61,7 +61,7 @@ params: dict[str, Any] = {
 
 # Define parameter sweep ranges
 param_sweep = {
-    "rows": [5],
+    "rows": [15],
     "win_length": [5],
     # "rows": [3, 5],
     # "win_length": [3, 4],
@@ -75,17 +75,8 @@ sweep_combinations = list(product(*param_sweep.values()))
 # Map parameter names to their respective indices in the combinations
 param_keys = list(param_sweep.keys())
 
-# Define a folder to save all models
-script_dir = Path(__file__).resolve().parent
-all_models_folder = (script_dir / '../models/all_models').resolve()
-if not all_models_folder.exists():
-    all_models_folder.mkdir(parents=True)
-
 # Create a list to store filenames and corresponding parameter values
 model_metadata = []
-
-# Save the metadata to a JSON file after every iteration
-metadata_file = all_models_folder / "model_metadata.json"
 
 # Iterate over each parameter combination
 for sweep_idx, combination in enumerate(sweep_combinations):
@@ -109,12 +100,16 @@ for sweep_idx, combination in enumerate(sweep_combinations):
 
     if params["load_network"]:
         script_dir = Path(__file__).resolve().parent
-        relative_folder = (script_dir / '../models/all_models').resolve()
-        model_path_X = f"{relative_folder}/q_network_3x3x3_X_weights.pth"
-        model_path_O = f"{relative_folder}/q_network_3x3x3_O_weights.pth"
+        relative_folder = (script_dir / '../models/foundational').resolve()
+        model_path_X = f"{relative_folder}/q_network_5x5x5_X_weights.pth"
+        model_path_O = f"{relative_folder}/q_network_5x5x5_O_weights.pth"
 
         paramsX["load_network"] = model_path_X
         paramsO["load_network"] = model_path_O
+
+        print(f"Loading model from {model_path_X} and {model_path_O}")
+        if not os.path.exists(model_path_X) or not os.path.exists(model_path_O):
+            raise FileNotFoundError(f"Model files {model_path_X} or {model_path_O} do not exist.")
 
     outcomes = {"X": 0, "O": 0, "D": 0}
 
@@ -148,12 +143,24 @@ for sweep_idx, combination in enumerate(sweep_combinations):
 
     finally:
         if params["save_models"]:
+            
+            # Define a folder to save all models
+            script_dir = Path(__file__).resolve().parent
+            all_models_folder = (script_dir / '../models/all_models').resolve()
+            if not all_models_folder.exists():
+                all_models_folder.mkdir(parents=True)
+
+            # Save the metadata to a JSON file after every iteration
+            metadata_file = all_models_folder / "model_metadata.json"
+
             # Save the models with recognizable filenames
             model_X_filename = f"q_network_{rows}x{rows}x{win_length}_X.pth"
             model_O_filename = f"q_network_{rows}x{rows}x{win_length}_O.pth"
 
             torch.save(learning_agent1.q_network, all_models_folder / model_X_filename) # type: ignore
             torch.save(learning_agent2.q_network, all_models_folder / model_O_filename) # type: ignore
+            
+            print(f"Models saved as {model_X_filename} and {model_O_filename}")
 
             # Save the models weights with recognizable filenames
             model_X_filename = f"q_network_{rows}x{rows}x{win_length}_X_weights.pth"
